573580828a5e596343ae3aadc80317d308147648
typ <beam.wannasiri@gmail.com>
2024-12-03 03:16:42 +0700

    Create custom animations

--- composeApp/src/commonMain/kotlin/com/typ/bookpedia/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/typ/bookpedia/app/App.kt
@@ -1,5 +1,7 @@
 package com.typ.bookpedia
 
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -35,7 +37,12 @@ fun App() {
             navigation<Route.BookGraph>(
                 startDestination = Route.BookList
             ){
-                composable<Route.BookList> {
+                composable<Route.BookList> (
+                    exitTransition = { slideOutHorizontally() },
+                    popEnterTransition = { slideInHorizontally()
+
+                    }
+                ){
                     val viewModel = koinViewModel<BookListViewModel>()
                     val selectedBookViewModel = it.sharedKoinViewModel<SelectedBookViewModel>(navController)
 
@@ -53,7 +60,14 @@ fun App() {
                         }
                     )
                 }
-                composable<Route.BookDetail> {
+                composable<Route.BookDetail>(
+                    enterTransition = { slideInHorizontally{ initailOffset ->
+                        initailOffset
+                    } },
+                    exitTransition = { slideOutHorizontally{ initailOffset ->
+                        initailOffset
+                    } }
+                ) {
                     val selectedBookViewModel = it.sharedKoinViewModel<SelectedBookViewModel>(navController)
                     val viewModel = koinViewModel<BookDetailViewModel>()
                     val selectedBook by selectedBookViewModel.selectedBook.collectAsStateWithLifecycle()
